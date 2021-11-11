7f3c1803009a693aa9ac2422c8e653776ceb5cb2
Mymaqn <jensfox999@live.dk>
2021-11-11 19:57:32 +0100

    Didn't work trying something else

--- install_minimal.sh
+++ install_minimal.sh
@@ -33,39 +33,30 @@ pacstrap /mnt base linux linux-firmware
 #Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
+#Create file to run within chroot environment
+echo "ln -sf /usr/share/zoneinfo/Europe/Copenhagen /etc/localtime" >> /mnt/continue.sh
+echo "hwclock --systohc" >> /mnt/continue.sh
+echo "sed -i -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen" >> /mnt/continue.sh
+echo "sed -i -e 's/#en_US ISO-8859-1/en_US ISO-8859-1/g' /etc/locale.gen" >> /mnt/continue.sh
+echo "locale-gen" >> /mnt/continue.sh
+echo "echo \"LANG=EN_US.UTF-8\" > /etc/locale.conf" >> /mnt/continue.sh
+echo "echo \"zopzop\" > /etc/hostname" >> /mnt/continue.sh
+echo "echo \"127.0.1.1  zopzop\">> /etc/hosts" >> /mnt/continue.sh
+
+echo "pacman -Sy networkmanager" >> /mnt/continue.sh
+echo "useradd -G wheel -m zopazz" >> /mnt/continue.sh
+echo "echo \"Set user password\"" >> /mnt/continue.sh
+echo "passwd zopazz" >> /mnt/continue.sh
+
+echo "pacman -Sy grub efibootmgr" >> /mnt/continue.sh
+echo "pacman -Sy os-prober" >> /mnt/continue.sh
+echo "grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB" >> /mnt/continue.sh
+echo "grub-mkconfig -o /boot/grub/grub.cfg" >> /mnt/continue.sh
+
+echo "echo \"Set root password\"" >> /mnt/continue.sh
+echo "passwd" >> /mnt/continue.sh
+echo "exit" >> /mnt/continue.sh
+echo "reboot now" >> /mnt/continue.sh
+
 #Go chroot in
-arch-chroot /mnt
-
-#time and locale setup
-ln -sf /usr/share/zoneinfo/Europe/Copenhagen /etc/localtime
-
-hwclock --systohc
-
-sed -i -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
-sed -i -e 's/#en_US ISO-8859-1/en_US ISO-8859-1/g' /etc/locale.gen
-
-locale-gen
-echo "LANG=EN_US.UTF-8" > /etc/locale.conf
-
-#hostname and hosts file
-echo "zopzop" > /etc/hostname
-echo "127.0.1.1  zopzop">> /etc/hosts
-
-pacman -Sy networkmanager
-
-#Create user
-useradd -G wheel -m zopazz
-echo "Set user password"
-passwd zopazz
-
-#install grub
-pacman -Sy grub efibootmgr
-pacman -Sy os-prober
-
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-echo "Set root password"
-passwd
-exit
-reboot now
+arch-chroot /mnt /bin/bash -c "chmod +x /continue.sh;/continue.sh"
