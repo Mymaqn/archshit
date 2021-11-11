a6e8e8b16c51cb0854c014afe366a7db290e495b
Mymaqn <jensfox999@live.dk>
2021-11-11 20:56:50 +0100

    Trying again

--- install_minimal.sh
+++ install_minimal.sh
@@ -3,9 +3,11 @@ timedatectl set-ntp true
 lsblk
 echo "Create a main drive as Linux Filesystem and a 4gig swap type partition"
 echo "If you are going to be dual booting, take note of your efi partition"
+echo "If you are not dual booting create an efi partition at the start of your drive size 512M"
 read -p "What drive whould you like to install on?" installdrive
 cfdisk $installdrive
 
+read -p "EFI partition name:" efipart
 read -p "Filesystem partition name:" ext4Part
 read -p "Swap partition name:" swapPart
 
@@ -17,15 +19,9 @@ mount $ext4Part /mnt
 
 #Check if they are dual booting. If they are we want to mount their efi partition to /mnt/efi
 mkdir /mnt/efi
-
-read -p "Dual boot? (y/N)" dualboot
-case $dualboot in
-
-y|Y|yes|Yes|YES)
-
-read -p "Name of efi partition from Windows" efipart
 mount $efipart /mnt/efi
-esac
+
+
 
 #install base system
 pacstrap /mnt base linux linux-firmware
