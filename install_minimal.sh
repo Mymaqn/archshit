7bcd19f27747e5b1fa2af6313b753965e8cbf568
Mymaqn <jensfox999@live.dk>
2021-11-11 21:49:12 +0100

    Added some quality of life features like extra prompts and that it adds my configure script at the end

--- install_minimal.sh
+++ install_minimal.sh
@@ -1,14 +1,43 @@
 timedatectl set-ntp true
+echo "------------------------------------------------------------------------"
 lsblk
+echo "------------------------------------------------------------------------"
 echo "Create a main drive as Linux Filesystem and a 4gig swap type partition"
-echo "If you are going to be dual booting, take note of your efi partition"
+echo "If you are going to be dual booting, take note of your efi partition from the list above"
 echo "If you are not dual booting create an efi partition at the start of your drive size 512M"
+echo ""
+echo "Example for a fresh install partitioning:"
+echo "Drive size: 300gb"
+echo "EFI partition: 512M"
+echo "Linux FileSystem partition: 295.5G"
+echo "Swap partition: 4G"
+echo ""
+echo "Example for a dual boot partitioning"
+echo "Drive size: 300gb"
+echo "Linux Filesystem parition: 296G"
+echo "Swap parition: 4G"
+echo ""
 read -p "What drive whould you like to install on?" installdrive
 cfdisk $installdrive
 
+echo "------------------------------------------------------------------------"
 read -p "EFI partition name:" efipart
 read -p "Filesystem partition name:" ext4Part
 read -p "Swap partition name:" swapPart
+echo "------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------"
+echo "You have chosen:" 
+echo "EFI partition: $efipart"
+echo "Filesystem partition/ext4 $ext4Part"
+echo "Swap partition $swapPart"
+
+echo ""
+echo "These partitions will now be overwritten by a new filesystem and get formatted"
+read -p "Are you sure you want to continue? (y/N)" $format
+echo "------------------------------------------------------------------------"
+
+case $format in
+y/Y/yes/Yes)
 
 mkfs.ext4 $ext4Part
 mkfs.fat -F 32 $efipart
@@ -38,21 +67,35 @@ echo "echo \"LANG=EN_US.UTF-8\" > /etc/locale.conf" >> /mnt/continue.sh
 echo "echo \"zopzop\" > /etc/hostname" >> /mnt/continue.sh
 echo "echo \"127.0.1.1  zopzop\">> /etc/hosts" >> /mnt/continue.sh
 
-echo "pacman -Sy networkmanager" >> /mnt/continue.sh
+echo "pacman -Sy networkmanager git sudo vim wget --noconfirm" >> /mnt/continue.sh
+echo "systemctl enable NetworkManager" >> /mnt/continue.sh
 echo "useradd -G wheel -m zopazz" >> /mnt/continue.sh
 echo "echo \"Set user password\"" >> /mnt/continue.sh
 echo "passwd zopazz" >> /mnt/continue.sh
 
-echo "pacman -Sy grub efibootmgr" >> /mnt/continue.sh
-echo "pacman -Sy os-prober" >> /mnt/continue.sh
+echo "pacman -Sy grub efibootmgr --noconfirm" >> /mnt/continue.sh
+echo "pacman -Sy os-prober --noconfirm" >> /mnt/continue.sh
 echo "echo \"GRUB_DISABLE_OS_PROBER=false\" >> /etc/default/grub" >> /mnt/continue.sh
 echo "grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB" >> /mnt/continue.sh
 echo "grub-mkconfig -o /boot/grub/grub.cfg" >> /mnt/continue.sh
 
 echo "echo \"Set root password\"" >> /mnt/continue.sh
+echo "echo \"git clone https://github.com/Mymaqn/archshit\" >> /home/zopazz/configure.sh" >> /mnt/continue.sh
+echo "echo \"cd archshit\" >> /home/zopazz/configure.sh" >> /mnt/continue.sh
+echo "echo \"chmod +x ./configuration.sh >> /home/zopazz/configure.sh" >> /mnt/continue.sh
+echo "echo \"./configuration.sh\"" >> /mnt/continue.sh
 echo "passwd" >> /mnt/continue.sh
 echo "exit" >> /mnt/continue.sh
-echo "reboot now" >> /mnt/continue.sh
+
+
 
 #Go chroot in
 arch-chroot /mnt /bin/bash -c "chmod +x /continue.sh;/continue.sh"
+rm /mnt/continue.sh
+echo "Rebooting in 3 seconds.." && sleep 3
+reboot now
+;;
+*)
+echo "Aborting"
+esac
+
