a126c00d6b17935c2d4cd97dd8d0d0038bb9ec27
Mymaqn <jensfox999@live.dk>
2021-11-11 22:46:27 +0100

    Small cleanup along with adding user to sudoers file (needs debugging)

--- install_minimal.sh
+++ install_minimal.sh
@@ -60,36 +60,49 @@ y|Y|yes|Yes)
     genfstab -U /mnt >> /mnt/etc/fstab
 
     #Create file to run within chroot environment
+    #Sync time
     echo "ln -sf /usr/share/zoneinfo/Europe/Copenhagen /etc/localtime" >> /mnt/continue.sh
     echo "hwclock --systohc" >> /mnt/continue.sh
+    
+    #Set locale
     echo "sed -i -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen" >> /mnt/continue.sh
     echo "sed -i -e 's/#en_US ISO-8859-1/en_US ISO-8859-1/g' /etc/locale.gen" >> /mnt/continue.sh
     echo "locale-gen" >> /mnt/continue.sh
     echo "echo \"LANG=EN_US.UTF-8\" > /etc/locale.conf" >> /mnt/continue.sh
+
+    #Create hostname
     echo "echo \"zopzop\" > /etc/hostname" >> /mnt/continue.sh
     echo "echo \"127.0.1.1  zopzop\">> /etc/hosts" >> /mnt/continue.sh
 
+    #Install my need to have applciations and enable internet
     echo "pacman -Sy networkmanager git sudo vim wget --noconfirm" >> /mnt/continue.sh
     echo "systemctl enable NetworkManager" >> /mnt/continue.sh
+
+    #Create new user and add them to the sudoers file along with everyone in the wheel group
     echo "useradd -G wheel -m zopazz" >> /mnt/continue.sh
     echo "echo \"Set user password\"" >> /mnt/continue.sh
     echo "passwd zopazz" >> /mnt/continue.sh
-
+    echo "sed -i -e ':a;N;\$!ba;s/## Uncomment to allow members of group wheel to execute any command\n#%wheel ALL=(ALL) ALL/## Uncomment to allow members of group wheel to execute any command\n%wheel ALL=(ALL) ALL/g'"
+    
+    #Setup grub
     echo "pacman -Sy grub efibootmgr --noconfirm" >> /mnt/continue.sh
     echo "pacman -Sy os-prober --noconfirm" >> /mnt/continue.sh
-    #echo "echo \"GRUB_DISABLE_OS_PROBER=false\" >> /etc/default/grub" >> /mnt/continue.sh
+    echo "echo \"GRUB_DISABLE_OS_PROBER=false\" >> /etc/default/grub" >> /mnt/continue.sh
     echo "grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB" >> /mnt/continue.sh
     echo "grub-mkconfig -o /boot/grub/grub.cfg" >> /mnt/continue.sh
 
-    
+    #Create script that can be run on launch which downloads and runs the configuration I like
     echo "echo \"git clone https://github.com/Mymaqn/archshit\" >> /home/zopazz/configure.sh" >> /mnt/continue.sh
     echo "echo \"cd archshit\" >> /home/zopazz/configure.sh" >> /mnt/continue.sh
     echo "echo \"chmod +x ./configuration.sh\" >> /home/zopazz/configure.sh" >> /mnt/continue.sh
     echo "echo \"./configuration.sh\" >> /home/zopazz/configure.sh" >> /mnt/continue.sh
+    
+    #Set the root password
     echo "echo \"Set root password\"" >> /mnt/continue.sh
     echo "passwd" >> /mnt/continue.sh
     echo "exit" >> /mnt/continue.sh
-    #Go chroot in
+    
+    #Now chroot in and execute the above script then remove it, when done
     arch-chroot /mnt /bin/bash -c "chmod +x /continue.sh;/continue.sh"
     rm /mnt/continue.sh
     read -p "Press any key to reboot" 
@@ -99,4 +112,6 @@ y|Y|yes|Yes)
     echo "Aborting"
     ;;
 esac
+
+
 
