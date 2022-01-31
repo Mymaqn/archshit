929de02c393afd552a2dc645a8201e41070ff019
Zopazz <jensfox999@live.dk>
2022-01-31 15:37:22 +0100

    Added kde-plasma theme

--- configuration.sh
+++ configuration.sh
@@ -42,6 +42,16 @@ cp wallpaper.png ~/Pictures
 ./set-wallpaper.sh
 
 
+#Set the global KDE theme:
+tar xf ./kde-theme/Iridescent-kvantum.tar.gz -C ~/.local/share/plasma/look-and-feel/
+lookandfeeltool -a Iridescent-kvantum
+
+#Add the Konsole color scheme:
+cp ./konsole/Shiny-Konsole.colorscheme ~/.local/share/konsole/Shiny-Konsole.colorscheme
+cp ./zopzop.profile ~/.local/share/konsole/zopzop.profile
+cp ./konsole/konsolerc ~/.config/konsolerc
+
+
 #Installation of zsh with cool stuff WOOP
 zsh /usr/share/zsh/functions/Newuser/zsh-newuser-install -f
 
