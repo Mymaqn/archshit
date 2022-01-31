f11026adc4fdecfca454cc62ea6219b7640645f1
Zopazz <jensfox999@live.dk>
2022-01-31 15:57:08 +0100

    Fixed a bug which stopped themes from applying in set-themes

--- set-themes.sh
+++ set-themes.sh
@@ -4,10 +4,11 @@ cp wallpaper.png ~/Pictures
 
 
 #Set the global KDE theme:
+mkdir ~/.local/share/plasma/look-and-feel
 tar xf ./kde-theme/Iridescent-kvantum.tar.gz -C ~/.local/share/plasma/look-and-feel/
 lookandfeeltool -a Iridescent-kvantum
 
 #Add the Konsole color scheme:
 cp ./konsole/Shiny-Konsole.colorscheme ~/.local/share/konsole/Shiny-Konsole.colorscheme
-cp ./zopzop.profile ~/.local/share/konsole/zopzop.profile
+cp ./konsole/zopzop.profile ~/.local/share/konsole/zopzop.profile
 cp ./konsole/konsolerc ~/.config/konsolerc
