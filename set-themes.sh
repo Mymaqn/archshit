1e6742d484400c8d807569521d99c75fdb5a5fe0
Zopazz <jensfox999@live.dk>
2022-01-31 16:11:53 +0100

    Added BeautyLine icons to repo

--- set-themes.sh
+++ set-themes.sh
@@ -7,6 +7,8 @@ cp wallpaper.png ~/Pictures
 #Set the global KDE theme:
 mkdir ~/.local/share/plasma
 mkdir ~/.local/share/plasma/look-and-feel
+mkdir ~/.local/share/icons
+tar xf ./kde-theme/BeautyLine.tar.gz -C ~/.local/share/icons
 tar xf ./kde-theme/Iridescent-kvantum.tar.gz -C ~/.local/share/plasma/look-and-feel/
 lookandfeeltool -a Iridescent-kvantum
 
