b9d927f048801a1938b58d6b0985d1c094f5b3fb
Zopazz <jensfox999@live.dk>
2022-01-31 17:05:08 +0100

    More themes stuff

--- set-themes.sh
+++ set-themes.sh
@@ -8,6 +8,14 @@ cp wallpaper.png ~/Pictures
 mkdir ~/.local/share/plasma
 mkdir ~/.local/share/plasma/look-and-feel
 mkdir ~/.local/share/icons
+mkdir ~/.local/share/color-schemes
+mkdir ~/.local/share/plasma/desktoptheme
+mkdir ~/.kde4/share/apps
+mkdir ~/.kde4/share/apps/color-schemes
+
+tar xf ./kde-theme/Iridescent-round.tar.gz -C ~/.local/share/plasma/desktoptheme
+tar xf ./kde-theme/color-schemes.tar.gz -C ~/.local/share/
+cp ~/.local/share/color-schemes/IridescentLightly3.colors ~/.kd4/share/apps/color-schemes/
 tar xf ./kde-theme/BeautyLine.tar.gz -C ~/.local/share/icons
 tar xf ./kde-theme/Iridescent-kvantum.tar.gz -C ~/.local/share/plasma/look-and-feel/
 lookandfeeltool -a Iridescent-kvantum
